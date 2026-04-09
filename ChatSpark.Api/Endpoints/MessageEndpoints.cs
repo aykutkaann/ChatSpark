a77faea87bf99814c85aaee0fdf3799c70cc3d50
aykutkaann <aykut8112@gmail.com>
2026-04-10 02:30:25 +0300

    feat: Added GET messages with BEFORE and LIMIT.

--- ChatSpark.Api/Endpoints/MessageEndpoints.cs
+++ ChatSpark.Api/Endpoints/MessageEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using ChatSpark.Domain.Entities;
 using ChatSpark.Infrastructure.Persistence;
 using ChatSpark.Shared.Dtos.Messages;
+using Dapper;
 using Microsoft.EntityFrameworkCore;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -48,6 +49,57 @@ namespace ChatSpark.Api.Endpoints
                     messages.Content,
                     messages.SentAt,
                     messages.EditedAt));
+
+            });
+
+
+            group.MapGet("/", async (
+                Guid channelId,
+                DateTime? before,
+                int? limit,
+                AppDbContext db,
+                IDbConnectionFactory connectionFac,
+                ClaimsPrincipal principal) =>
+            {
+
+                var userId = Guid.Parse(principal.FindFirst(JwtRegisteredClaimNames.Sub)?.Value);
+
+                var channel = await db.Channels
+                        .AsNoTracking()
+                        .FirstOrDefaultAsync(c => c.Id == channelId);
+                if (channel is null) return Results.NotFound("Channel not found.");
+
+                var isWorkspaceMember = await db.WorkspaceMembers.AnyAsync(m => m.WorkspaceId == channel.WorkspaceId && m.UserId == userId);
+
+                if (!isWorkspaceMember) return Results.Forbid();
+
+                if (channel.IsPrivate)
+                {
+                    var isChannelMember = await db.ChannelMembers.AnyAsync(c => c.ChannelId == channel.Id && c.UserId == userId);
+
+                    if (!isChannelMember) return Results.Forbid();
+                }
+
+                var effectiveLimit = Math.Clamp(limit ?? 50, 1, 100);
+
+                const string sql = @"
+                                SELECT id, channel_id, sender_id, content, sent_at, edited_at
+                                FROM messages
+                                WHERE channel_id = @ChannelId 
+                                  AND (@Before::timestamptz IS NULL OR sent_at < @Before::timestamptz)
+                                ORDER BY sent_at DESC
+                                LIMIT @Limit;";
+
+
+                using var conn = await connectionFac.CreateAsync();
+                var messages = await conn.QueryAsync<MessageResponse>(sql, new
+                {
+                    ChannelId = channelId,
+                    Before = before,
+                    Limit = effectiveLimit
+                });
+
+                return Results.Ok(messages);
 
             });
         }
